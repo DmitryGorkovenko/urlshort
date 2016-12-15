ac345889d5289a1408351d9b8f9660e0428187c3
Gorkovenko Dmitry <Dmitry.Gorkovenko@infotecs.ru>
2016-12-15 15:48:27 +0300

    настройки для SQL

--- settings.py
+++ settings.py
@@ -5,4 +5,7 @@ class Config(object):
     ROOT_PATH = os.path.dirname(__file__)
     LOG_PATH = os.path.join(ROOT_PATH, 'logs/request.log')
     TIME_ZONE = 'Europe/Moscow'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_POOL_SIZE = 100
+    SQLALCHEMY_POOL_RECYCLE = 200
     SQLALCHEMY_DATABASE_URI = "sqlite:///url.db"
